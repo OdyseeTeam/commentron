1c1f9fd58f1f3700d008c5e7f5ca6629ff420a43
Mark Beamer Jr <markbeamerjr@gmail.com>
2021-02-15 21:07:45 -0500

    test CI

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+## test ci
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 docker build --tag lbry/commentron:$TRAVIS_BRANCH ./
 docker push lbry/commentron:$TRAVIS_BRANCH
