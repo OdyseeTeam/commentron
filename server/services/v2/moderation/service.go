a800af0d4eb84bb516943333300f68906b784e71
Mark Beamer Jr <markbeamerjr@gmail.com>
2021-05-24 13:56:37 -0400

    put blocked list back in, removed accidentally.

--- server/services/v2/moderation/service.go
+++ server/services/v2/moderation/service.go
@@ -24,6 +24,11 @@ func (s Service) UnBlock(r *http.Request, args *commentapi.UnBlockArgs, reply *c
 	return unBlock(r, args, reply)
 }
 
+// BlockedList return the list of blocked channels for a moderator
+func (s Service) BlockedList(r *http.Request, args *commentapi.BlockedListArgs, reply *commentapi.BlockedListResponse) error {
+	return blockedList(r, args, reply)
+}
+
 // AddDelegate return the list of blocked channels for a moderator
 func (s Service) AddDelegate(r *http.Request, args *commentapi.AddDelegateArgs, reply *commentapi.ListDelegateResponse) error {
 	return addDelegate(r, args, reply)
