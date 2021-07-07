b6c1acde00f6458a3974bf94e0b17798a7584c4d
Mark Beamer Jr <markbeamerjr@gmail.com>
2021-07-07 18:31:33 -0400

    Add API field for accepting Fiat Tip id for confirmation as super chat.

--- commentapi/comment.go
+++ commentapi/comment.go
@@ -53,15 +53,16 @@ type EditResponse struct {
 
 // CreateArgs arguments for the comment.Create rpc call
 type CreateArgs struct {
-	CommentText string  `json:"comment"`
-	ClaimID     string  `json:"claim_id"`
-	ParentID    *string `json:"parent_id"`
-	ChannelID   string  `json:"channel_id"`
-	ChannelName string  `json:"channel_name"`
-	SupportTxID *string `json:"support_tx_id"`
-	SupportVout *uint64 `json:"support_vout"`
-	Signature   string  `json:"signature"`
-	SigningTS   string  `json:"signing_ts"`
+	CommentText     string  `json:"comment"`
+	ClaimID         string  `json:"claim_id"`
+	ParentID        *string `json:"parent_id"`
+	ChannelID       string  `json:"channel_id"`
+	ChannelName     string  `json:"channel_name"`
+	SupportTxID     *string `json:"support_tx_id"`
+	SupportVout     *uint64 `json:"support_vout"`
+	PaymentIntentID *string `json:"payment_intent_id"`
+	Signature       string  `json:"signature"`
+	SigningTS       string  `json:"signing_ts"`
 }
 
 // CreateResponse response for the comment.Create rpc call
