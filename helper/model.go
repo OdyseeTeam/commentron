d8062facb9bad8ac75ed26e274c5588cffab77c5
Mark Beamer Jr <markbeamerjr@gmail.com>
2021-11-17 00:07:53 -0500

    cache channel find and creator settings retrieval

--- helper/model.go
+++ helper/model.go
@@ -2,20 +2,40 @@ package helper
 
 import (
 	"database/sql"
+	"time"
 
 	"github.com/lbryio/commentron/db"
-
-	"github.com/volatiletech/null"
-
 	"github.com/lbryio/commentron/model"
 
 	"github.com/lbryio/lbry.go/v2/extras/errors"
 
+	"github.com/karlseguin/ccache"
+	"github.com/volatiletech/null"
 	"github.com/volatiletech/sqlboiler/boil"
 )
 
+var channelCache = ccache.New(ccache.Configure().GetsPerPromote(1).MaxSize(100000))
+
 // FindOrCreateChannel gets the channel from commentron database or creates it and returns it
 func FindOrCreateChannel(channelClaimID, channelName string) (*model.Channel, error) {
+	item, err := channelCache.Fetch(channelClaimID, 1*time.Hour, func() (interface{}, error) {
+		channel, err := getChannel(channelClaimID, channelName)
+		if err != nil {
+			return nil, err
+		}
+		return channel, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	c, ok := item.Value().(*model.Channel)
+	if !ok {
+		return nil, errors.Err("could not convert item to channel")
+	}
+	return c, nil
+}
+
+func getChannel(channelClaimID, channelName string) (*model.Channel, error) {
 	channel, err := model.Channels(model.ChannelWhere.ClaimID.EQ(channelClaimID)).One(db.RO)
 	if errors.Is(err, sql.ErrNoRows) {
 		if channelName == "" {
@@ -34,8 +54,28 @@ func FindOrCreateChannel(channelClaimID, channelName string) (*model.Channel, er
 	return channel, errors.Err(err)
 }
 
+var settingsCache = ccache.New(ccache.Configure().GetsPerPromote(1).MaxSize(100000))
+
 // FindOrCreateSettings gets the settings for the creator from commentron database or creates it and returns it
 func FindOrCreateSettings(creatorChannel *model.Channel) (*model.CreatorSetting, error) {
+	item, err := settingsCache.Fetch(creatorChannel.ClaimID, 1*time.Hour, func() (interface{}, error) {
+		setting, err := getSettings(creatorChannel)
+		if err != nil {
+			return nil, err
+		}
+		return setting, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	setting, ok := item.Value().(*model.CreatorSetting)
+	if !ok {
+		return nil, errors.Err("could not convert item to creator setting")
+	}
+	return setting, nil
+}
+
+func getSettings(creatorChannel *model.Channel) (*model.CreatorSetting, error) {
 	settings, err := creatorChannel.CreatorChannelCreatorSettings().One(db.RO)
 	if errors.Is(err, sql.ErrNoRows) {
 		settings = &model.CreatorSetting{CreatorChannelID: creatorChannel.ClaimID, CommentsEnabled: null.BoolFrom(true)}
@@ -45,5 +85,5 @@ func FindOrCreateSettings(creatorChannel *model.Channel) (*model.CreatorSetting,
 			return nil, errors.Err(err)
 		}
 	}
-	return settings, errors.Err(err)
+	return settings, nil
 }
