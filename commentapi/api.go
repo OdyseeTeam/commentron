f1754435467e5c986c420946508f7b22f1e8baf9
Mark Beamer Jr <markbeamerjr@gmail.com>
2021-12-29 13:17:51 -0500

    update something in the commentapi package for go get

--- commentapi/api.go
+++ commentapi/api.go
@@ -2,7 +2,7 @@ package commentapi
 
 import "github.com/lbryio/lbry.go/extras/api"
 
-// Validator for api arguments that should have some checks applied for every request
+// Validator for api arguments that should have some checks applied for every request.
 type Validator interface {
 	Validate() api.StatusError
 }
