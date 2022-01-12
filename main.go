f8311879541860db5ca1864b6567ab8a0c4a0cfa
Mark Beamer Jr <markbeamerjr@gmail.com>
2022-01-11 23:19:32 -0500

    allow bindata file to be committed to repo

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import "github.com/lbryio/commentron/cmd"
 
-//go:generate go-bindata -o migration/bindata.go -pkg migration -ignore bindata.go migration/
+//go:generate go-bindata -o migration/bindata.go -nometadata -pkg migration -ignore bindata.go migration/
 //go:generate go fmt ./migration/bindata.go
 //go:generate goimports -l ./migration/bindata.go
 
