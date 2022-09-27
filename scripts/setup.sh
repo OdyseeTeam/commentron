45c3269099d0e8e0f4fe7833901d87a9a2984bf0
tom <thomas.zarebczan@gmail.com>
2022-09-27 15:42:07 -0400

    Auto stash before merge of "creator-settings-new" and "origin/creator-settings-new"

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export DEBUGGING=1
-export MYSQL_DSN_RO="lbry-ro:lbry@tcp(localhost:3306)/commentron"
-export MYSQL_DSN_RW="lbry-rw:lbry@tcp(localhost:3306)/commentron"
+export MYSQL_DSN_RO="odysee:thomas@tcp(localhost:3306)/commentron"
+export MYSQL_DSN_RW="odysee:thomas@tcp(localhost:3306)/commentron"
 touch -a .env && set -o allexport; source ./.env; set +o allexport
