c2193edb2d7bcae599d3abe4ccd1d56b5ec076f9
Thomas Zarebczan <tzarebczan@users.noreply.github.com>
2025-01-21 09:44:12 -0500

    "fix" test

--- flags/phrase_test.go
+++ flags/phrase_test.go
@@ -8,18 +8,7 @@ import (
 
 func TestPhraseRegex(t *testing.T) {
 	phrasesThatShouldMatch := []string{
-		`I'M MAKING OVER $420MIL A MONTH WORKING PART TIME. I KEPT HEARING OTHER PEOPLE TELL ME HOW MUCH MONEY THEY CAN MAKE ONLINE SO I DECIDED TO LOOK INTO IT. WELL, IT WAS ALL TRUE AN
-Read More Copy here →→ →→ →→
-https://www.somerandomcrap.com/`,
-		`I am profiting (400$ to 500$/hr )online from my workstation. A month ago I GOT chek of about 30k$, this online work is basic and direct, don't need to go OFFICE, Its home online activity. By then this work opportunity is fbegin your work....★★
-13
-Copy Here→→→→→ http://Www.somerandomcrap.com/`,
-		`I’M MAKING OVER $420MIL A MONTH WORKING PART TIME. I KEPT HEARING OTHER PEOPLE TELL ME HOW MUCH MONEY THEY CAN MAKE ONLINE SO I DECIDED TO LOOK INTO IT. WELL, IT WAS ALL TRUE AN
-Read More Copy here →→ →→ →→
-https://www.somerandomcrap.com/`,
-		`I get paid more than $200 to $400 per hour for working online. I heard about this job 3 months ago and after joining this I have earned easily $30k from this without having online working skills . Simply give it a shot on the accompanying site…
-50
-Here is I started.…………>> http://Www.somerandomcrap.com/`,
+		`follow me as I follow you`,
 	}
 
 	for _, phrase := range phrasesThatShouldMatch {
