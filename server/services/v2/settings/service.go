8c157cc10b580229e6e426f29184bc5419b9b1eb
miko <sauce47@posteo.net>
2025-01-26 21:16:38 +0200

    Apply the settings

--- server/services/v2/settings/service.go
+++ server/services/v2/settings/service.go
@@ -110,6 +110,22 @@ func (s *Service) Update(r *http.Request, args *commentapi.UpdateSettingsArgs, r
 		}
 	}
 
+	if args.MinUSDCTipAmountSuperChat != nil {
+		cents := *args.MinUSDCTipAmountComment * 100
+		settings.MinUSDCTipAmountSuperChat.SetValid(uint64(cents))
+		if *args.MinUSDCTipAmountSuperChat == 0.0 {
+			settings.MinUSDCTipAmountSuperChat.Valid = false
+		}
+	}
+
+	if args.MinUSDCTipAmountComment != nil {
+		cents := *args.MinUSDCTipAmountComment * 100
+		settings.MinUSDCTipAmountComment.SetValid(uint64(cents))
+		if *args.MinUSDCTipAmountComment == 0.0 {
+			settings.MinUSDCTipAmountComment.Valid = false
+		}
+	}
+
 	if args.CurseJarAmount != nil { // Coming with Appeal process
 		settings.CurseJarAmount.SetValid(*args.CurseJarAmount)
 		if *args.CurseJarAmount == 0.0 {
@@ -259,6 +275,14 @@ func applySettingsToReply(settings *model.CreatorSetting, reply *commentapi.List
 	if settings.MinTipAmountSuperChat.Valid {
 		minTipAmountSuperChat := btcutil.Amount(settings.MinTipAmountSuperChat.Uint64).ToBTC()
 		reply.MinTipAmountSuperChat = &minTipAmountSuperChat
+	}
+	if settings.MinUSDCTipAmountComment.Valid {
+		minUSDCTipAmountComment := float64(settings.MinUSDCTipAmountComment.Uint64) / float64(100)
+		reply.MinUSDCTipAmountComment = &minUSDCTipAmountComment
+	}
+	if settings.MinUSDCTipAmountSuperChat.Valid {
+		minUSDCTipAmountSuperChat := float64(settings.MinUSDCTipAmountSuperChat.Uint64) / float64(100)
+		reply.MinUSDCTipAmountSuperChat = &minUSDCTipAmountSuperChat
 	}
 	if settings.SlowModeMinGap.Valid {
 		reply.SlowModeMinGap = &settings.SlowModeMinGap.Uint64
